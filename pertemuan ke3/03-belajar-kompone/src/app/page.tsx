70b6b4d78db7a2bbe589471e9e9cd3237bd4d1d4
BntnggShll <bintangsuhel25@gmail.com>
2024-02-29 00:03:43 +0700

    W03: Jawaban soal 7,8,9

--- pertemuan ke3/03-belajar-kompone/src/app/page.tsx
+++ pertemuan ke3/03-belajar-kompone/src/app/page.tsx
@@ -1,14 +1,21 @@
-import { Gallery } from "@/components/gallery";
-import TodoList from "@/components/todolist";
+import Image from "next/image";
+import { Gallery } from "../components/gallery";
+import TodoList from "../components/todolist";
+import MyGallery from "../components/mygallery";
+import MyAvatar from "../components/myprofilev2";
 
 export default function Home() {
   return (
     <section>
-      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
+      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuan yang luar biasa</h1>
       <hr />
       <Gallery />
       <hr />
       <TodoList />
+      <hr />
+      <MyGallery />
+      <hr />
+      <MyAvatar />
     </section>
   );
 }
