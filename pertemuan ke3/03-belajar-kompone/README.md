16651d5050323fb398d9ad494b685f6b7f339691
BntnggShll <bintangsuhel25@gmail.com>
2024-02-28 20:46:59 +0700

    W03: Jawaban soal 2

---
+++ pertemuan ke3/03-belajar-kompone/README.md
@@ -0,0 +1,25 @@
+**Praktek 1**
+Langkah 1 :Buat Komponen Baru
+![Step-1](img/1.png)
+
+Langkah 2 : Import Komponen
+![Step-2](img/7.png)
+
+
+**Praktek 2**
+Langkah 1 :Buat Komponen Baru
+![Step-1](img/3.png)
+
+Langkah 2 : Import Komponen
+![Step-2](img/6.png)
+
+
+
+**Soal**
+Soal 1
+![Soal-1](img/5.png)
+
+Soal 2
+![Soal-2](img/8.png)
+
+
--- pertemuan ke3/03-belajar-kompone/src/app/page.tsx
+++ pertemuan ke3/03-belajar-kompone/src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
-import Profile from "../components/profile";
+import { Gallery } from "@/components/gallery";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-80">
       <h1 className="mb-4">Ilmuan yang luar biasa</h1>
       <div className="flex space-x-4">
-        <Profile />
-        <Profile />
-        <Profile />
+        <Gallery />
+        <Gallery />
+        <Gallery />
       </div>
     </main>
   );
