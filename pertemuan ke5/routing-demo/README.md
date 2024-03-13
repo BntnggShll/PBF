a6b31e725af9a3690c1a97c906c7bcbe979217d5
BntnggShll <bintangsuhel25@gmail.com>
2024-03-13 09:36:30 +0700

    Praktikum 1

---
+++ pertemuan ke5/routing-demo/README.md
@@ -0,0 +1,10 @@
+Praktek
+
+Halaman Utama
+![Step-1](images/p1l1.png)
+
+Halaman About
+![Step-2](images/p1l2.png)
+
+Halaman Profile
+![Step-3](images/p1l3.png)
---
+++ pertemuan ke5/routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Nama  : Bintang Suhel</h1>
+      <h1>Nim   : 2341728037</h1>
+      <h1>Kelas :TI3C</h1>
+    </div>
+  );
+}
